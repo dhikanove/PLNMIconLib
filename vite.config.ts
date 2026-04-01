13bccb63e09231d55072cfff292f03d91edde924
Andhika Nove Rezki <andhikanoverezki@MacBook-Pro-Andhika.local>
2026-04-01 15:15:08 +0700

    add command build demo

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,39 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/icons/index.ts'),
-      name: 'PlnmIconLib',
-      fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      // Vue must be listed as external — consumers provide it
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
+export default defineConfig(({ mode }) => {
+  // 🔥 BUILD LIBRARY
+  if (mode === 'lib') {
+    return {
+      plugins: [vue()],
+      build: {
+        lib: {
+          entry: resolve(__dirname, 'src/icons/index.ts'),
+          name: 'PlnmIconLib',
+          fileName: (format) => `index.${format === 'es' ? 'js' : 'cjs'}`,
+          formats: ['es', 'cjs'],
         },
-        exports: 'named',
+        rollupOptions: {
+          external: ['vue'],
+          output: {
+            globals: {
+              vue: 'Vue',
+            },
+            exports: 'named',
+          },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
       },
+    }
+  }
+
+  // 🌐 BUILD DEMO
+  return {
+    plugins: [vue()],
+    build: {
+      outDir: 'docs',
+      emptyOutDir: true,
     },
-    sourcemap: true,
-    emptyOutDir: true,
-  },
+  }
 })
